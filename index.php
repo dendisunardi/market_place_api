b65802ad54974d9bb413ea1ab060b581455aab47
dendisunardi <dendi.sunardi@gmail.com>
2017-05-05 23:16:16 +0700

    menambahkan label password

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 Login<hr width="25%" align="left"><br>
 <form name="form_login" method="POST" action="action/login.php">
 Username kamu: <input type="text" name="emil"/><br />
-Password : <input type="password" name="pssword"/><br />
+Password kamu : <input type="password" name="pssword"/><br />
 <input type="submit" value="Login"/>
 </form>
 </body>
